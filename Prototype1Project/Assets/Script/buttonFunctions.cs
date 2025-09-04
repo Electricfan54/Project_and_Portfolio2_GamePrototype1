e45009faac72b75332baf33dee13470013c0a817
Jose <149850498+DevPixil1@users.noreply.github.com>
2025-09-04 15:55:41 -0400

    UI Setup and GameManager made

---
+++ Prototype1Project/Assets/Script/buttonFunctions.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    
+    public void Resume()
+    {
+        gameManager.instance.UnpauseGame();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameManager.instance.UnpauseGame();
+    }
+
+    public void Quit()
+    {
+
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+            Application.Quit();
+    #endif
+
+    }
+
+}

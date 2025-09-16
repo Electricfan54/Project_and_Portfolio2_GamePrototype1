a4ef521a42019334ed0698ca7589f08ec729d5d7
Gabriel <gabriel.m.alicea@gmail.com>
2025-09-16 15:41:35 -0400

    updated WeaponScript

--- Prototype1Project/Assets/Script/WeaponScript.cs
+++ Prototype1Project/Assets/Script/WeaponScript.cs
@@ -10,8 +10,12 @@ public class WeaponScript : ScriptableObject
 
     // Gun stuff
     [Range(1, 50)] public int gunDamage;
-    [Range(0.1f, 2)] public float fireRate;
+    [Range(0.1f, 5)] public float fireRate;
     [Range(1, 10)] public int DestroyTime;
+    [Range(1, 50)] public int currAmmo;
+    [Range(1, 50)] public int clipSize;
+    [Range(1, 250)] public int maxAmmo;
+    [Range(0.1f, 10)] public float ReloadTimer;
 
     // Gernade stuff
     [Range(0.1f, 5)] public float explodeTime;

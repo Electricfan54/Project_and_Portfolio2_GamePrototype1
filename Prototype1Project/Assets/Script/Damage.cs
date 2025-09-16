e1e29fe751ca0b12f2bf4435eea9e08c8506d4fb
lazybonez2006 <lazybonez2006@gmail.com>
2025-09-16 16:13:41 -0400

    fixing poison and lava

--- Prototype1Project/Assets/Script/Damage.cs
+++ Prototype1Project/Assets/Script/Damage.cs
@@ -3,24 +3,40 @@ using System.Collections;
 
 public class Damage : MonoBehaviour
 {
-    enum DamageType { moving, stationary, explosion, DOT, Homing }
+    enum DamageType { moving, stationary, explosion, DOT, Homing, poision }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] Rigidbody rb;
+
     public int damageamount;
     public float damageRate;
     public int speed;
     public int destroyTime;
-    public float explodetime;
     [SerializeField] DamageType type;
+    [Tooltip("Ammount of time before explosion")]
+    public float explodetime;
+    [Tooltip("expolsion radius/ homing projectile lock on radius")]
     public int radius;
     [SerializeField] SphereCollider explosioncollider;
-    public float homingPauseTime;
-    public float hominggotime;
     [SerializeField] GameObject explosionEffect;
     float explodetimer;
-    bool isDamaging;
+
+    [Tooltip("how long the homing bullet stops for")]
+    public float homingPauseTime;
+    [Tooltip("how long the pullet goes for after detecting a target")]
+    public float hominggotime;
+
     Vector3 homingTarget;
+    [Tooltip("the speed the projectile will move after finding an enemy")]
+    public float speedAfterStop;
     float homingTimer;
+
+    [Tooltip("Poison things")]
+    bool ispoison;
+    public int posionwaitbeforhit;
+    public int PoisonTimesHit;
+    bool isDamaging;
+
+
     bool canDamage = false;
     void Start()
     {
@@ -128,6 +144,14 @@ public class Damage : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+        IDamage dmg = other.GetComponent<IDamage>();
+        if (type==DamageType.poision)
+        {
+            if(dmg!=null)
+            {
+                StartCoroutine(PoisonLeave(dmg));
+            }
+        }
     }
     // Update is called once per frame
     void Update()
@@ -153,6 +177,15 @@ public class Damage : MonoBehaviour
             }
 
         }
+        if (dmg != null && type == DamageType.poision)
+        {
+            if (!isDamaging)
+            {
+                StartCoroutine(damageother(dmg));
+            }
+
+        }
+
 
 
     }
@@ -179,17 +212,34 @@ public class Damage : MonoBehaviour
     }
     IEnumerator HomingDelay()
     {
-        yield return new WaitForSeconds(hominggotime);
+        yield return new WaitForSeconds(hominggotime); // Time it goes before it stops
         rb.linearVelocity = Vector3.zero;
         gameObject.GetComponent<SphereCollider>().radius = 0.1f;
-        yield return new WaitForSeconds(homingPauseTime);
+        yield return new WaitForSeconds(homingPauseTime);// Time it stops for
         Quaternion rot = Quaternion.LookRotation(homingTarget - transform.position);
         transform.rotation = rot;
-        rb.linearVelocity = transform.forward * speed;
+        rb.linearVelocity = transform.forward * speedAfterStop; //speed after stoping
 
         canDamage = true;
         homingTimer = 0;
 
     }
+    IEnumerator Poisonenter(IDamage d)
+    {
+        isDamaging = true;
+        d.TakeDamage(damageamount);
+        yield return new WaitForSeconds(damageRate);
+        isDamaging = false;
+    }
+    IEnumerator PoisonLeave(IDamage d)
+    {
+        isDamaging = true;
+        for (int i = 0; i < PoisonTimesHit; i++)
+        {
+            d.TakeDamage(damageamount);
+            yield return new WaitForSeconds(posionwaitbeforhit);
+        }
+        isDamaging = false;
+    }
 
-}
+    }

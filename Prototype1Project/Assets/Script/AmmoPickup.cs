070be75d0d9b2b7970d16fe856a790edb4cbe509
Electricfan54 <electivefan54@gmail.com>
2025-09-22 14:55:39 -0400

    Ammo pickups now use their respective sprite.

--- Prototype1Project/Assets/Script/AmmoPickup.cs
+++ Prototype1Project/Assets/Script/AmmoPickup.cs
@@ -4,6 +4,13 @@ public class AmmoPickup : MonoBehaviour
 {
     public ammoType type;
     public int amount;
+    [SerializeField] GameObject model;
+    [SerializeField] float modelRotateSpeed;
+
+    private void Update()
+    {
+        model.transform.Rotate(0, modelRotateSpeed * Time.deltaTime, 0);
+    }
 
     private void OnTriggerEnter(Collider other)
     {

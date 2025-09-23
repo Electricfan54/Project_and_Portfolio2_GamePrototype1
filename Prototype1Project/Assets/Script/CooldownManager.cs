02e71ea2b2f31c20bbd3028f6ba2cf02c1251288
Gabriel <gabriel.m.alicea@gmail.com>
2025-09-23 13:30:11 -0400

    UpdateUI

---
+++ Prototype1Project/Assets/Script/CooldownManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CooldownManager : MonoBehaviour
+{
+    [SerializeField] Vector2 CoolDownPos1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
